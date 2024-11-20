6e50aaaaec095ad91ccba1f8155be27f7bc9878a
Alexandr P.V. <alexpoidenko@gmail.com>
2024-11-20 14:06:42 +0100

    fix: product page build issue

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,11 @@
-"use client";
-
 import { ProductPage } from "@/components/app-products-id-page";
+import { use } from "react";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
 
-export default function Page({ params }: PageProps) {
+export default function Page(props: PageProps) {
+  const params = use(props.params);
   return <ProductPage productId={params.id} />;
 }
